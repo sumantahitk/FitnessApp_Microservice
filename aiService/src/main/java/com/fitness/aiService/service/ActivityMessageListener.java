c59b64439f8e5ca9f20a2b29adac29a221754c55
Sumanta Rakshit <sumantarakshit6299@gmail.com>
2026-02-19 16:21:36 +0530

    AI generated Recommendation Completed

--- aiService/src/main/java/com/fitness/aiService/service/ActivityMessageListener.java
+++ aiService/src/main/java/com/fitness/aiService/service/ActivityMessageListener.java
@@ -1,6 +1,8 @@
 package com.fitness.aiService.service;
 
 import com.fitness.aiService.model.Activity;
+import com.fitness.aiService.model.Recommendation;
+import com.fitness.aiService.repository.RecommendationRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
@@ -13,9 +15,16 @@ import org.springframework.stereotype.Service;
 public class ActivityMessageListener {
 
 
+    private final ActivityAIService activityAIService;
+    private final RecommendationRepository recommendationRepository;
+
     @KafkaListener(topics = "${kafka.topic.name}",groupId = "activity-processor-group")
     public void processActivity(Activity activity){
         log.info("Received Activity for processing: {}",activity.getUserId());
+        Recommendation recommendation=activityAIService.generateRecommendation(activity);
+
+        log.info("AI GENERATED RECOMMENDATION: {}",recommendation);
+        recommendationRepository.save(recommendation);
 
     }
 
--- aiService/src/main/java/com/fitness/aiService/service/GeminiService.java
+++ aiService/src/main/java/com/fitness/aiService/service/GeminiService.java
@@ -1,6 +1,5 @@
 package com.fitness.aiService.service;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -8,7 +7,6 @@ import org.springframework.web.reactive.function.client.WebClient;
 import java.util.Map;
 
 @Service
-//@RequiredArgsConstructor
 public class GeminiService {
     private final WebClient webClient;
 
